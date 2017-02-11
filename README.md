86b3184dc7f18b0964b60e097becc9e6101f4d49
Rajesh Raikwar <rajesh.raikwar1211@gmail.com>
2017-02-12 01:27:07 +0530

    form validations added

---
+++ README.md
@@ -0,0 +1,6 @@
+# Movie Recommender App
+Simple android app for movie recommendations based on ML(WIP) algorithm.
+
+![MovieRecommender](/public/device-2017-02-12-012350.png?raw=true "MovieRecommender")
+
+![MovieRecommender](/public/device-2017-02-12-012423?raw=true "MovieRecommender")
--- app/src/main/java/in/pipecast/movierecommender/UserInfoActivity.java
+++ app/src/main/java/in/pipecast/movierecommender/UserInfoActivity.java
@@ -5,7 +5,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,13 +17,20 @@ import java.util.List;
 
 public class UserInfoActivity extends AppCompatActivity {
 
+    private EditText user_name;
+    private EditText age;
+    private RadioGroup gender_radio_group;
+    private RadioButton gender_radio;
+    private Spinner spinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
-        Spinner spinner = (Spinner) findViewById(R.id.occupation_spinner);
+        spinner = (Spinner) findViewById(R.id.occupation_spinner);
 
         List<String> occupations = new ArrayList<String>();
+        occupations.add("Select");
         occupations.add("Engineer");
         occupations.add("Artist");
         occupations.add("Doctor");
@@ -33,37 +44,68 @@ public class UserInfoActivity extends AppCompatActivity {
     public void getRecommendations(View v) {
 
         if (validateUserData()) {
-            final ProgressDialog progress=new ProgressDialog(this);
-            progress.setMessage("Hang on! Crunching data for you..");
-            progress.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-            progress.setIndeterminate(false);
-            progress.setProgress(0);
-            progress.show();
 
-            final int totalProgressTime = 100;
-            final Thread t = new Thread() {
-                @Override
-                public void run() {
-                    int jumpTime = 0;
+            //Later main recommendation engine logic goes here
+            System.out.println(user_name.getText());
+            System.out.println(age.getText());
+            System.out.println(gender_radio.getText());
+            System.out.println(spinner.getSelectedItem().toString());
+            //Temp ProgressBar
+            showProgress();
 
-                    while(jumpTime < totalProgressTime) {
-                        try {
-                            sleep(2000);
-                            jumpTime += 20;
-                            progress.setProgress(jumpTime);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    progress.dismiss();
-                }
-            };
-            t.start();
         }
     }
 
-    public boolean validateUserData() {
+    public void showProgress() {
+        final ProgressDialog progress=new ProgressDialog(this);
+        progress.setMessage("Hang on! Crunching data for you..");
+        progress.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progress.setIndeterminate(false);
+        progress.setProgress(0);
+        progress.show();
+        final int totalProgressTime = 100;
+        final Thread t = new Thread() {
+            @Override
+            public void run() {
+                int jumpTime = 0;
 
+                while(jumpTime < totalProgressTime) {
+                    try {
+                        sleep(2000);
+                        jumpTime += 20;
+                        progress.setProgress(jumpTime);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                progress.dismiss();
+            }
+        };
+        t.start();
+    }
+
+    public boolean validateUserData() {
+        user_name = (EditText) findViewById(R.id.user_name);
+        age = (EditText) findViewById(R.id.user_age);
+        gender_radio_group = (RadioGroup)findViewById(R.id.gender_radio_group);
+        gender_radio = (RadioButton) findViewById( gender_radio_group.getCheckedRadioButtonId());
+
+        if (user_name.getText().length() == 0) {
+            user_name.setError("Please enter username");
+            return false;
+        }
+        if (age.getText().length() == 0) {
+            age.setError("Please enter age");
+            return false;
+        }
+        if (gender_radio == null) {
+            Toast.makeText(this, "Please select your gender", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (spinner.getSelectedItem().toString() == "Select" ){
+            Toast.makeText(this, "Please select your occupation", Toast.LENGTH_SHORT).show();
+            return false;
+        }
         return true;
     }
 
