f22abb6767d99330662b59b1b35c8a0c9a1e91c4
Rajesh Raikwar <rajesh.raikwar1211@gmail.com>
2017-02-11 14:58:07 +0530

    ic launcher , home page and place holder for main page included

--- app/src/main/java/in/pipecast/movierecommender/MainActivity.java
+++ app/src/main/java/in/pipecast/movierecommender/MainActivity.java
@@ -1,13 +1,27 @@
 package in.pipecast.movierecommender;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+    }
+
+    public void go_to_main_form(View v) {
+        Intent i = new Intent(this, UserInfoActivity.class);
+        startActivity(i);
     }
 }
